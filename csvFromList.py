0049e1ff9425cf0bf5dd628140581cc8a3f2ceac
AnchalChandraGupta <anchalchandraguptaanchal@gmail.com>
2017-01-17 15:51:21 +0530

    Update csvFromList.py

--- csvFromList.py
+++ csvFromList.py
@@ -1,16 +1,28 @@
 import csv
 
-input = [('\'app,le\' r,e\'d','b','c','d'),('e','f','g','h'),('i','j','k','l'),('m','n','o','p'),('q','r','s','t'),('u','v','w','x')]
+# write from records list to csv file
+def writeToCSV(fileName, recordsList):
+    with open(fileName, 'w',  newline='') as csvfile:
+        writer = csv.writer(csvfile,  delimiter=',', quotechar = '"', quoting = csv.QUOTE_ALL)
+        for tuple in recordsList:
+            writer.writerow(tuple)
+    csvfile.close()
 
-with open('/home/hanshika/Desktop/aaaaaaa.csv', 'w',  newline='') as csvfile:
-    writer = csv.writer(csvfile,  delimiter=',', quotechar = '"', quoting = csv.QUOTE_MINIMAL)
-    for tuple in input:
-        writer.writerow(tuple)
-csvfile.close()
+#read from csv file and print to standard output
+def readFromCSV(fileName):
+    with open(fileName,  newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            print(row)
+    csvfile.close()
 
-with open('/home/hanshika/Desktop/aaaaaaa.csv',  newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-    for row in reader:
-        print(','.join(row))
+#input list of tuples
+records = [('column1','column2','column3','colum4'),
+         ('aaaaaaa','bbbbbbb','ccccccc','dd\'dd"dd,d'),
+         ('\'eeeeeee\'','"fffffff"','gg"gggg"g','hh?!@h#$%^&*('),
+         ('     _a','     ','null','NULL'),
+         (1234567,'5565.234','..67837','abce123')]
 
+writeToCSV('/home/hanshika/Desktop/aaaaaaa.csv',records)
+readFromCSV('/home/hanshika/Desktop/aaaaaaa.csv')
 
